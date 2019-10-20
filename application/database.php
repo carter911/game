143f5f9fd7d4cdff3cf312cd5c07a7da30fd9fb2
carter911 <chenrj123456>
2019-10-20 14:08:53 +0800

    commit

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.94.201.246',
+    'hostname'        => 'localhost',
     // 数据库名
     'database'        => 'game',
     // 用户名
     'username'        => 'game',
     // 密码
-    'password'        => 'BHPAH5WMNbhS4GNN',
+    'password'        => 'HH7wbAed5fCx4CNr',
     // 端口
     'hostport'        => '',
     // 连接dsn
