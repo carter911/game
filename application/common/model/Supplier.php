ed4d357c73b96cb90a41ffa2d69eb84e9f027595
chenrj <7299216972qq.com>
2019-11-22 11:29:19 +0800

    初始化

--- application/common/model/Supplier.php
+++ application/common/model/Supplier.php
@@ -85,8 +85,9 @@ class Supplier extends Model
             return $res;
         }
         $data['create_at'] = time();
-        dump($data);die;
+
         $res = $this->allowField(true)->insert($data);
+        dump($data);die;
         if (empty($res)) {
             return false;
         }
