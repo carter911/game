f7bf8e2e330ba3d971a35d0178f438805bdc97f2
carter911 <chenrj123456>
2019-11-23 17:44:01 +0800

    add log

--- application/common/gateway/Exchange.php
+++ application/common/gateway/Exchange.php
@@ -10,6 +10,7 @@ class Exchange extends Base
 {
     protected $base_url = '';
     public $order = [];
+
     public function __construct($orderId = 0)
     {
         $model = new Order();
@@ -21,30 +22,29 @@ class Exchange extends Base
     public static function formatPlatform($p)
     {
         $platform = [
-            'FFA20PS4'=>'ps4',
-            'FFA20XBO'=>'xbox',
+            'FFA20PS4' => 'ps4',
+            'FFA20XBO' => 'xbox',
         ];
-        return isset($platform[$p])?$platform[$p]:'ps4';
+        return isset($platform[$p]) ? $platform[$p] : 'ps4';
     }
 
 
-    public  $formatPlatformBySupplier = [
-        'ps4'=>'FFA20PS4',
-        'xbox'=>'FFA20XBO',
+    public $formatPlatformBySupplier = [
+        'ps4' => 'FFA20PS4',
+        'xbox' => 'FFA20XBO',
     ];
     //Alex / JvbJeyCoLQJ   API key: BtDsk86rCJ7H8nvOaDsVnPE64oCntdk
-
 
 
     const KEY = 'BtDsk86rCJ7H8nvOaDsVnPE64oCntdk';
     const USER_ID = 'Alex';
     const PGW_URL = 'https://fifa-exchange.com/';
 
-    public static function getParam($action='getPrices')
+    public static function getParam($action = 'getPrices')
     {
         $param = [
-            'username'=>self::USER_ID,
-            'hash'=>md5(self::USER_ID.self::KEY),
+            'username' => self::USER_ID,
+            'hash' => md5(self::USER_ID . self::KEY),
         ];
         return $param;
     }
@@ -55,27 +55,27 @@ class Exchange extends Base
         $data = [];
         try {
             $params = [];
-            $url = self::PGW_URL.'getprices-ajax';
-            $res = self::curlPost($url,self::getParam(),$data);
-            if($res !=200){
-                Log::error('exchange远程请求地址'.$url.var_export($res,true).var_export($data,true));
+            $url = self::PGW_URL . 'getprices-ajax';
+            $res = self::curlPost($url, self::getParam(), $data);
+            if ($res != 200) {
+                Log::error('exchange远程请求地址' . $url . var_export($res, true) . var_export($data, true));
                 return false;
             }
 
-            $data = json_decode($data,true);
+            $data = json_decode($data, true);
             $price = [];
-            foreach ($data['prices'] as $key=> $item){
+            foreach ($data['prices'] as $key => $item) {
 
-                if(in_array($key,array_keys($this->formatPlatformBySupplier))){
-                    $price[$this->formatPlatformBySupplier[$key]] = round($item/1000,3);
-                    if($data['stock'][$key] <=0){
+                if (in_array($key, array_keys($this->formatPlatformBySupplier))) {
+                    $price[$this->formatPlatformBySupplier[$key]] = round($item / 1000, 3);
+                    if ($data['stock'][$key] <= 0) {
                         $price[$this->formatPlatformBySupplier[$key]] = 999;
                     }
                 }
             }
             return $price;
         } catch (\Throwable $e) {
-            Log::error('Exchange远程请求地址'.$e->getMessage());
+            Log::error('Exchange远程请求地址' . $e->getMessage());
             return [];
             //echo $e->getMessage();
         }
@@ -83,84 +83,64 @@ class Exchange extends Base
 
     public function balance()
     {
-        $params = [];
-        foreach (self::getParam('getAmountOwed') as $key => $val){
-            $params[] = $key."=".$val;
-        }
-        $params = implode("&",$params);
-        $url = self::PGW_URL.'?'.$params;
-        $res = self::curlJson($url,[],$data,[],'GET');
-        if($res !=200){
-            Log::error('Utloader远程请求地址'.$url.var_export($res,true).var_export($data,true));
-            return false;
-        }
-        return $data;
+
+        return [];
     }
 
     public function newOrder($orderInfo)
     {
-        if(empty($orderInfo)){
+        if (empty($orderInfo)) {
             return false;
         }
-
-        $params = self::getParam('addOrder');
+        $params = self::getParam();
         $params['email'] = trim($orderInfo['login']);
         $params['password'] = trim($orderInfo['password']);
         $params['platform'] = self::formatPlatform($orderInfo['platform']);
-        $params['backup_code'] = $orderInfo['backup1'];
-        $params['amount'] = $orderInfo['amount']/1000;
-        $param = [];
-        foreach ($params as $key => $val){
-            $param[] = $key."=".$val;
+        $params['backup'] = json_encode([$orderInfo['backup1'],$orderInfo['backup2'],$orderInfo['backup3']]);
+        $params['amount'] = $orderInfo['amount'] / 1000;
+        $url = self::PGW_URL . 'getprices-ajax';
+        $res = self::curlPost($url, $params, $data);
+        if ($res != 200) {
+            Log::error('exchange远程请求地址' . $url . var_export($res, true) . var_export($data, true));
+            return false;
         }
-        $param = implode("&",$param);
-        $data = [];
-        $res = self::curlJson(self::PGW_URL."?".$param,[],$data,[],'GET');
-        $data['pgw_return'] = json_encode($data);
-        if($res !=200){
-            Log::error('Utloader远程请求地址'.self::PGW_URL.var_export($res,true).var_export($data,true));
-            return $data;
-        }
-
-        if($data['status'] ==1){
+        $data['pgw_return'] = ($data);
+        $data = json_decode($data, true);
+        if($data['code']  == 200){
             $data['status'] = 'transferring';
-        }else {
-            if($data['coins_remaining'] == $data['transfer_amount']){
-                $data['status'] = 'end';
-            }else{
-                $data['status'] = 'transferring';
-            }
+        }else{
+            Log::error('exchange返回异常' . $url . var_export($res, true) . var_export($data, true));
+            return false;
         }
-        $data['pgw_message'] = isset($data['reason'])?$data['reason']:'';
-        $data['pgw_order_id'] = isset($data['token'])?$data['token']:'';
+        $data['pgw_order_id'] = isset($data['orderID']) ? $data['orderID'] : '';
         return $data;
     }
 
     public function queryOrder($orderInfo)
     {
-        if(empty($orderInfo['pgw_prder_id'])){
+        if (empty($orderInfo['pgw_prder_id'])) {
             return false;
         }
         $params = self::getParam('trackOrder');
         $params['token'] = $orderInfo['pgw_prder_id'];
         $param = [];
-        foreach ($params as $key => $val){
-            $param[] = $key."=".$val;
+        foreach ($params as $key => $val) {
+            $param[] = $key . "=" . $val;
         }
-        $param = implode("&",$param);
+        $param = implode("&", $param);
         $data = [];
-        $res = self::curlJson(self::PGW_URL."?".$param,[],$data,[],'GET');
+        $res = self::curlJson(self::PGW_URL . "?" . $param, [], $data, [], 'GET');
         $data['pgw_return'] = json_encode($data);
-        if($res !=200){
-            Log::error('Utloader远程请求地址'.self::PGW_URL.var_export($res,true).var_export($data,true));
+        if ($res != 200) {
+            Log::error('Utloader远程请求地址' . self::PGW_URL . var_export($res, true) . var_export($data, true));
             return $data;
         }
-        if($data['result'] ==200){
-            if($data['coins_transferred'] == $data['transfer_amount']){
+        if ($data['result'] == 200) {
+            if ($data['coins_transferred'] == $data['transfer_amount']) {
                 $data['status'] = 'end';
             }
             $data['transaction_already_amount'] = $data['coins_transferred'];
-        }else{
+        } else {
             echo '订单不存在';
             //TODO 应该是没有找到这个订单
             Log::error($data);
