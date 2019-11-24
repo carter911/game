bdca186a06bfed627317f7a78b4a8e97ece2b710
carter911 <chenrj123456>
2019-11-24 13:17:01 +0800

    add log

--- application/cron/controller/Supplier.php
+++ application/cron/controller/Supplier.php
@@ -88,7 +88,7 @@ class Supplier
         $list = $order->where(['status'=>'transferring'])->order('id asc')->limit(5)->select();
         $list = $list->toArray();
         foreach ($list as $key=> $val){
-            echo "需要更新状态为".$val['id'].'上游网管'.$val['pgw_payment'].'<br/>';
+            echo "需要更新状态为".$val['pgw_order_id'].'上游网管'.$val['pgw_payment'].'<br/>';
             $gateway = "app\\common\\gateway\\".$val['pgw_payment'];
             $pgw = new $gateway();
             $res = $pgw->queryOrder($val);
