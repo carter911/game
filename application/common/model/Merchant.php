592693960f2e4613d95c19e27b0b5df5881d146e
chenrj <7299216972qq.com>
2019-11-25 18:23:28 +0800

    初始化

--- application/common/model/Merchant.php
+++ application/common/model/Merchant.php
@@ -44,7 +44,6 @@ namespace app\common\model;
 
      public function setStatusAttr($price)
      {
-         dump($price);
          $priceData = [];
          foreach ($this->gameType as $key=>$val){
              if(empty($price[$val])){
@@ -53,7 +52,6 @@ namespace app\common\model;
                  $priceData[$val] = $price[$val]=='on'?'online':'offline';
              }
          }
-         dump($priceData);die;
          return json_encode($priceData);
      }
 
