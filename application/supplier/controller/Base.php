8036aa0970e35ea45457a034d932dd746113388f
chenrj <7299216972qq.com>
2019-12-10 11:13:22 +0800

    添加取消订单操作

--- application/supplier/controller/Base.php
+++ application/supplier/controller/Base.php
@@ -2,6 +2,7 @@
 namespace app\supplier\controller;
 
 use think\Controller;
+use tp5redis\Redis;
 
 class Base extends Controller
 {
@@ -9,6 +10,8 @@ class Base extends Controller
     public function __construct()
     {
         parent::__construct();
+        $supplier = session('supplier');
+        Redis::hSet('request_supplier_log',time(),json_encode(['ip'=>$this->request->ip(),'info'=>$supplier,'request'=>$this->request->param()]));
         //$this->
     }
 
