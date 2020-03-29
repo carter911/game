2047caeb936f9c76d09902e829e2da632a728b87
carter911 <chenrj123456>
2020-03-29 15:15:15 +0800

    update Ct

--- application/database.php
+++ application/database.php
@@ -17,9 +17,9 @@ return [
     // 数据库名
     'database'        => 'game',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'game',
     // 密码
-    'password'        => '123456',
+    'password'        => 'HH7wbAed5fCx4CNr',
     // 端口
     'hostport'        => '',
     // 连接dsn
