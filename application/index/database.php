73b2f812f930515c2245b96aa8370354198b0ce7
chenrj <729921697@qq.com>
2020-08-18 18:37:17 +0800

    更新前端代码

--- application/index/database.php
+++ application/index/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '47.94.204.119',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'football',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'football',
     // 密码
-    'password'        => 'chenrj123',
+    'password'        => 'RTb3ZGHhPiMJkk2J',
     // 端口
     'hostport'        => '',
     // 连接dsn
